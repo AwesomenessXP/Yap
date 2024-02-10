43f58f2f63b96a2b147bdf809634ee2238e27671
haskmoney <heartstring85@gmail.com>
2024-02-09 20:17:21 -0800

    Initial Commit

---
+++ Yap/YapApp.swift
@@ -0,0 +1,17 @@
+//
+//  YapApp.swift
+//  Yap
+//
+//  Created by Haskell Macaraig on 2/9/24.
+//
+
+import SwiftUI
+
+@main
+struct YapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
