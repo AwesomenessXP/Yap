a31724b42f50eabfbaf8db3338ab3078fa10c980
YudiLai33 <yudi.lai2b@gmail.com>
2024-02-20 12:34:36 -0800

    modify root view: only add nav stack to setting page. add setting page UI, some cool down stuff

---
+++ Yap/SettingPage.swift
@@ -0,0 +1,19 @@
+//
+//  setting.swift
+//  Yap
+//
+//  Created by Yudi Lai on 2/20/24.
+//
+
+import Foundation
+import SwiftUI
+
+struct SettingPage: View {
+
+    var body: some View {
+        HStack{
+            Text("Setting")
+                .font(.title)
+        }
+    }
+}
