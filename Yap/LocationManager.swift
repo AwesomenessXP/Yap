613e00a51e0d98e4a126348db5e85ba58c273ab5
Haskell Macaraig <heartstring85@gmail.com>
2024-02-25 18:28:25 -0800

    updated text field

--- Yap/LocationManager.swift
+++ Yap/LocationManager.swift
@@ -19,14 +19,6 @@ class LocationManager: NSObject, ObservableObject, Observable, CLLocationManager
         print("\(manager.accuracyAuthorization)")
     }
     
-    func requestLocation() {
-        manager.startUpdatingLocation()
-    }
-    
-    func stopRequestLocation() {
-        manager.stopUpdatingLocation()
-    }
-    
     func locationManager(_ manager: CLLocationManager, didUpdateHeading newHeading: CLHeading) {
         degrees = newHeading.trueHeading
     }
